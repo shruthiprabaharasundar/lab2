d43e25e3ed91e8e8512eb86baeb704259843a018
ShruthiPrabaharasundar <prabaharasun@wisc.edu>
2023-09-25 09:40:37 -0500

    2nd commit

--- app/src/main/java/com/cs407/lab2/MainActivity.java
+++ app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -2,7 +2,12 @@ package com.cs407.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void clickFunction(View view){
+        EditText myTextField = (EditText) findViewById(R.id.editTextText);
+        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        goToActivity(myTextField.getText().toString());
+    }
+    public void goToActivity(String s){
+        Intent intent = new Intent(this, CalculatorActivity.class);
+        intent.putExtra("message",s);
+        startActivity(intent);
     }
 }
